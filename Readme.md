cc31106549f98750c67cab83786468f5babac3be
CCharlieLi <ccharlieli@live.com>
2016-04-12 14:44:05 +0800

    add readme

---
+++ Readme.md
@@ -0,0 +1,8 @@
+A python script to parse data from sensor like Nova SDS011 PM2.5 sensor
+
+
+- Edit `test.py` to suit your device
+- Test
+```
+$ python test.py
+```
--- test.py
+++ test.py
@@ -7,12 +7,4 @@ if __name__ == "__main__":
     while True:
         data = sensor.PM25_Hex(10).split(" ")
         pm = int(data[3]+data[2], 16)/10
-        #print 'PM2.5:',  pm
-        try:
-            if socket.gethostbyname("skyrover.com.cn") == '120.25.81.22':
-                r = requests.get("http://120.25.81.22:8000/idata/"+str(time.strftime("%H:%M:%S", time.localtime()))+"/"+str(pm)+"/",timeout=3)
-                print(r.url, r.text)
-        except Exception, e:
-            print e,  ': trying to reconnect in 3 seconds...'
-            time.sleep(3)
-            continue;
+        print 'PM2.5:',  pm
